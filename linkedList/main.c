dacce6241c82ef84fa50ae4a03a58fd8c93ef39d
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-16 19:44:33 +0300

    init linkedList

---
+++ linkedList/main.c
@@ -0,0 +1,64 @@
+#include "stdio.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node
+{
+    int val;
+    struct Node* next;
+};
+
+typedef struct {
+    struct Node* head;
+    struct Node* tail;
+    int size;
+    bool empty;
+} LinkedList;
+
+LinkedList* createList() {
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
+    list->size = 0;
+    list->empty = true;
+    list->head = NULL;
+    list->tail = NULL;
+
+    return list;
+}
+
+int size(LinkedList* list) {
+    return list->size;
+}
+
+void push_front(LinkedList* list, int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+
+    node->val = value;
+    node->next = NULL;
+
+    if (list->size == 0) {
+        list->head = node;
+        list->tail = node;
+    } else {
+        struct Node* curNode = list->head;
+
+        while (curNode->next)
+        {
+            curNode = curNode->next;
+        }
+
+        curNode->next = node;
+    }
+
+    list->size += 1;
+    list->empty = false;
+    list->tail = node;
+}
+
+void main() {
+   LinkedList* list = createList();
+   push_front(list, 3);
+   push_front(list, 4);
+   push_front(list, 65);
+
+   printf("%d", list->tail->val);
+}
