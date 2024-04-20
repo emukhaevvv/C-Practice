99b6edc11186b35533aec8fa3ba9c59d8fbde8b3
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-20 10:54:59 +0300

    push-front

--- linkedList/main.c
+++ linkedList/main.c
@@ -11,14 +11,15 @@ struct Node {
 struct LinkedList {
     int size;
     bool empty;
+    struct Node* head;
+    struct Node* tail;
 };
 
 struct LinkedList* createLinkedList();
+void push_front(struct LinkedList* list, int value);
 
 void main() {
     struct LinkedList* list = createLinkedList();
-
-    printf("%d", list->size);
 }
 
 struct LinkedList* createLinkedList() {
@@ -26,5 +27,27 @@ struct LinkedList* createLinkedList() {
     linkedList->size = 0;
     linkedList->empty = true;
 
+    push_front(linkedList, 54);
+    push_front(linkedList, 12);
+
     return linkedList;
+}
+
+void push_front(struct LinkedList* list, int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+
+    node->val = value;
+    node->next = NULL;
+
+    if (list->size == 0) {
+        list->head = node;
+        list->tail = node;
+
+        list->empty = false;
+    } else {
+        node->next = list->head;
+        list->head = node;  
+    }
+
+    list->size += 1;
 }
