ccc25646e2ffd813051146fc724304babe811ec6
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-20 10:41:57 +0300

    init list

--- linkedList/main.c
+++ linkedList/main.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #include <stdbool.h>
 
+struct Node {
+    int val;
+    struct Node* next;
+};
+
+struct LinkedList {
+    int size;
+    bool empty;
+};
+
+struct LinkedList* createLinkedList();
+
 void main() {
-    printf("Clear out");
+    struct LinkedList* list = createLinkedList();
+
+    printf("%d", list->size);
+}
+
+struct LinkedList* createLinkedList() {
+    struct LinkedList* linkedList = (struct LinkedList*)malloc(sizeof(struct LinkedList*));
+    linkedList->size = 0;
+    linkedList->empty = true;
+
+    return linkedList;
 }
