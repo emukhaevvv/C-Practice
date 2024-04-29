bfc518f6ce8a539ed621278561bcfb844fddc3d6
emukhaevvv <emukhaevvv@yandex.ru>
2024-04-29 14:05:08 +0300

    stack

---
+++ stack/main.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct Stack {
+    int* stack;
+    int size;
+    int max;
+};
+
+struct Stack* create_stack(int max) {
+    struct Stack* stack = (struct Stack*)malloc(sizeof( struct Stack));
+
+    stack->max = 4;
+    stack->size = -1;
+
+    stack->stack = (int*)malloc(4 * sizeof(int));
+
+    return stack;
+}
+
+void push(struct Stack* stack, int val) {
+    if (stack->max - 1 == stack->size) return;
+
+    stack->size += 1;
+    stack->stack[stack->size] = val;
+}
+
+void main() {
+    struct Stack* s = create_stack(4);
+}
